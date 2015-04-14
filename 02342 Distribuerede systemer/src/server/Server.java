d6bfbf41f37e1fc4d7c88bb143f64fa8468d7a3d
anders <anders@Bk-Studie>
2015-04-14 10:42:20 +0200

    she really does!

--- 02342 Distribuerede systemer/src/server/Server.java
+++ 02342 Distribuerede systemer/src/server/Server.java
@@ -1,7 +1,6 @@
 package server;
 
 import java.io.IOException;
-import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
